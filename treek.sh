4394a0496d4d2c768e835e55892cbc332a3e5b62
hakirot <hackirot@proton.me>
2022-01-10 00:04:08 -0800

    final

--- treek.sh
+++ treek.sh
@@ -5,9 +5,8 @@
 notify="inotifywait -r -e close_write -e access -e delete . &> /dev/null"
 
 clear
-cmd="tree -I node_modules -L 3 ."
+cmd="tree --noreport -I node_modules -L 3 ."
 $cmd
-
 
 while eval "$notify"; do
     clear
