ed684b2be5212d53fc06813698d5a7a6f445b321
hakirot <hackirot@proton.me>
2022-01-10 00:56:43 -0800

    Final

--- treek.sh
+++ treek.sh
@@ -2,13 +2,19 @@
 # install tree
 # install inotifytools
 
-clear
-
 notify="inotifywait @./node_modules -r -e close_write -e access -e delete . &> /dev/null"
-cmd="tree --noreport -I node_modules -L 3 ."
-$cmd
+cmd="tree -C --noreport -I node_modules -L 3 ."
+
+function run {
+	clear
+    printf '\033[1;30m'
+    echo "-= Treek =-"
+    printf '\033[0m'
+	$cmd
+}
+
+run
 
 while eval "$notify"; do
-    clear
-    $cmd
+    run
 done
