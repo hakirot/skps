08845e9fda2bfae6264f9870633ea85a43191a98
hakirot <hackirot@proton.me>
2025-03-29 19:49:13 -0700

    new colors

--- custom_walz/get-current.sh
+++ custom_walz/get-current.sh
@@ -3,5 +3,5 @@ set -eou pipefail
 
 GREEN='\033[0;32m'
 RESET='\033[0m' # No Color
-echo -e "Fetching ${GREEN}$HOME/.cache/wal/colors.json ${RESET}"
 cp $HOME/.cache/wal/colors.json .
+echo -e "Fetched ${GREEN}$HOME/.cache/wal/colors.json > ./colors.json${RESET}"
