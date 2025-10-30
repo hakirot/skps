44d10bb31f147650bbd7edb589f6e1a646cef390
hakirot <hackirot@proton.me>
2025-10-30 12:32:57 -0700

    another wallpaper, cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -2,4 +2,8 @@
 
 rm -rf $HOME/.cache/sxiv/
 rm -rf $HOME/.cache/ranger/
-rm -f $HOME/.cache/.bluetoothctl_history
+rm -f $HOME/.local/share/ranger/history
+rm -f $HOME/.cache/.bluetoothctl_history/
+rm -rf $HOME/.cache/thumbnails/
+rm -rf $HOME/.cache/vlc/
+rm -f $HOME/.local/share/nvim/telescope_history
