140a35ba7c97f8a586ddc4cf7866fa788fb9cafb
hakirot <hackirot@proton.me>
2025-12-14 21:18:17 -0800

    install theme and custom-embroidery

--- custom_walz/install_theme.sh
+++ custom_walz/install_theme.sh
@@ -2,21 +2,47 @@
 
 set -eou pipefail
 
-if [[ $EUID -ne 0 ]]; then
-   echo "Run as root." 
-   exit 1
-fi
+RED='\033[0;31m'
+RESET='\033[0m'
+#GREEN='\033[0;32m'
+#YELLOW='\033[0;33m'
 
-#if [ $# -ne 2 ]
-#then
-#  echo "Enter Filename"
-#  exit 1
-#fi
+INSTALL_DIR='/usr/lib/python3.13/site-packages/pywal/colorschemes/dark'
 
-if [ ! -f "$1" ]
+error() {
+  echo -e "${RED}"
+  echo "$@" 1>&2
+  echo -e "${RESET}"
+}
+
+print_help () {
+  echo 'Usage:'
+  echo '        ./install_theme.sh [filename]'
+  echo -e "${RED}Installing to $INSTALL_DIR${RESET}"
+}
+
+if [ $# -ne 1 ]
 then
-  echo "Filename does not exist"
+  print_help
   exit 1
 fi
 
-cp $1 /usr/lib/python3.13/site-packages/pywal/colorschemes/dark
+if [[ $EUID -ne 0 ]]; then
+   error "Run as root."
+   exit 1
+fi
+
+if [ ! -f "$1" ]
+then
+  error "Filename does not exist"
+  exit 1
+fi
+
+if [ ! -d "$INSTALL_DIR" ]
+then
+  error "Install directory does not exist"
+  exit 1
+fi
+
+
+cp $1 $INSTALL_DIR
