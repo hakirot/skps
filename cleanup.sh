8282a5ad3139c92f830ccd22f2f03d9a604b7684
hakirot <hackirot@proton.me>
2026-02-01 19:00:09 -0800

    updates

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,10 @@
 
 rm -rf $HOME/.cache/sxiv/
 rm -rf $HOME/.cache/ranger/
+rm -rf $HOME/.cache/sara
 rm -f $HOME/.local/share/ranger/history
+rm -f $HOME/.local/share/ranger/bookmarks
+rm -f $HOME/.local/share/recently-used.xbel
 rm -f $HOME/.cache/.bluetoothctl_history/
 rm -rf $HOME/.cache/thumbnails/
 rm -rf $HOME/.cache/vlc/
