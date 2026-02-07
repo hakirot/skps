8018ceb97e8d689bb9261c8a21a3dde09e799465
hakirot <hackirot@proton.me>
2026-02-07 11:17:05 -0800

    improve colorfetching

--- custom_walz/get-current.sh
+++ custom_walz/get-current.sh
@@ -4,4 +4,11 @@ set -eou pipefail
 GREEN='\033[0;32m'
 RESET='\033[0m'
 cp $HOME/.cache/wal/colors.json .
-echo -e "Fetched ${GREEN}$HOME/.cache/wal/colors.json > ./colors.json${RESET}"
+
+if  [[ $# -eq 1 ]]; then
+  file=$1
+  echo -e "Fetched ${GREEN}$HOME/.cache/wal/colors.json > ./${file}${RESET}"
+  mv colors.json $1
+else
+  echo -e "Fetched ${GREEN}$HOME/.cache/wal/colors.json > ./colors.json${RESET}"
+fi
