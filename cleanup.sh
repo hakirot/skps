01f4a2a279df9f2f611508f6d6bc21c5fe7455ce
hakirot <hackirot@proton.me>
2026-02-11 21:30:27 -0800

    updates

--- cleanup.sh
+++ cleanup.sh
@@ -6,10 +6,12 @@ rm -rf $HOME/.cache/sara
 rm -f $HOME/.local/share/ranger/history
 rm -f $HOME/.local/share/ranger/bookmarks
 rm -f $HOME/.local/share/recently-used.xbel
-rm -f $HOME/.cache/.bluetoothctl_history/
+rm -f $HOME/.cache/.bluetoothctl_history
 rm -rf $HOME/.cache/thumbnails/
 rm -rf $HOME/.cache/vlc/
 rm -f $HOME/.local/share/nvim/telescope_history
 rm -f $HOME/.host_zsh
 rm -f $HOME/.wget-hsts
 rm -f $HOME/dls/session/rtorrent.input_history
+rm -rf $HOME/.cache/ueberzugpp
+rm -rf $HOME/.cache/ueberzug
