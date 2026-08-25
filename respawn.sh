aff3e3d08ddcba05fd734dc01117741d6994881c
hakirot <hackirot@proton.me>
2026-08-25 03:40:26 -0700

    updates

--- respawn.sh
+++ respawn.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 
-nohup bash -c "$HOME/skps/reskin -r" 2&>1 /dev/null
-nohup bash -c "sleep 1; xdotool key super+space" 2&>1 /dev/null
+nohup bash -c "$HOME/skps/reskin -r" 2>&1 >/dev/null &
+nohup bash -c "sleep 1; xdotool key super+space" 2>&1 >/dev/null &
 
 if [[ -z "${VAR}" ]]; then
-  nohup bash -c "sleep .5; xdotool key super+q"
+  ppid=${PPID}
+  pppid=$(ps -o ppid= -p ${ppid})
+  ppppid=$(ps -o ppid = -p ${pppid})
+  nohup bash -c "sleep .1; kill -9 \"${ppppid}\"" &
+else
+  nohup bash -c "sleep .1; xdotool key super+q" 2>&1 >/dev/null &
   tmux kill-session
 else
-  echo ${PPID} > $HOME/ppid
 fi
