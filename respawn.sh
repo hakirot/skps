611d8d4cc457e285c84be405cc0cef1dd922e117
hakirot <hackirot@proton.me>
2026-08-25 03:54:22 -0700

    tweak

--- respawn.sh
+++ respawn.sh
@@ -8,8 +8,8 @@ if [ -z "${TMUX+x}" ]; then
   ppid=${PPID}
   pppid=$(ps -o ppid= -p ${ppid})
   ppppid=$(ps -o ppid = -p ${pppid})
-  nohup bash -c "sleep .1; kill -9 \"${ppppid}\"" &
+  nohup bash -c "sleep .2; kill -9 \"${ppppid}\"" &
 else
-  nohup bash -c "sleep .1; xdotool key super+q" 2>&1 >/dev/null &
+  nohup bash -c "sleep .2; xdotool key super+q" 2>&1 >/dev/null &
   tmux kill-session
 fi
