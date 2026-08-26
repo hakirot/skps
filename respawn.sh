db4789b77866ed4f9aa467e7e81a79c2e46f59be
hakirot <hackirot@proton.me>
2026-08-26 15:27:55 -0700

    updates

--- respawn.sh
+++ respawn.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 nohup bash -c "$HOME/skps/reskin -r" 2>&1 >/dev/null &
 nohup bash -c "sleep 1; xdotool key super+space" 2>&1 >/dev/null &
 
---
+++ song-notify.sh
@@ -0,0 +1,37 @@
+#!/bin/env bash
+
+# This should work, it has always worked for me, no guarantees though.
+# but please report mistakes to me :)
+
+music_dir="$HOME/mzk"
+fallback_image=/home/roe/pix/falling.png
+
+
+find_cover () {
+
+    ext="$(mpc --format %file% current | sed 's/^.*\.//')"
+    if [ "$ext" != "flac" ]; then
+        ffmpeg -y -i "$(mpc --format "$music_dir"/%file% | head -n 1)" \
+        /tmp/cover.jpg && cover_path="/tmp/cover.jpg" && return
+    else
+        # ffmpeg doesn't work for flacs
+        metaflac --export-picture-to=/tmp/cover.jpg \
+        "$(mpc --format "$music_dir"/%file% current)" && cover_path="/tmp/cover.jpg" && return
+    fi
+
+    # fallback
+    file="$music_dir/$(mpc --format %file% current)"
+    artist="$(mpc --format %artist% current)"
+    album="${file%/*}"
+
+    # search for cover
+    cover_path=$(find "$album"  -maxdepth 1 | grep -m 1 ".*\.\(jpg\|png\|gif\|bmp\)")
+}
+
+send () {
+    notify-send --expire-time 3000 -i "${cover_path}" "${artist}" "$(mpc current)" || \
+    notify-send --expire-time 3000 -i "${fallback_image}" "Now Playing:" "$(mpc current)"
+}
+
+find_cover 2>/dev/null
+send       2>/dev/null
